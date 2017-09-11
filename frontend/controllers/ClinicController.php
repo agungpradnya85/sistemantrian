270ef5f7ec9144ac3207df37073ce536817cd8a4
agung pradnya <agungpradnya85@gmail.com>
2017-09-11 08:58:17 +0800

    commit for register feature

---
+++ frontend/controllers/ClinicController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use common\models\Citizen;
+use common\models\CitizenSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * CitizenController implements the CRUD actions for Citizen model.
+ */
+class CitizenController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Citizen models.
+     * @return mixed
+     */
+     public function actionIndex()
+    {
+        return $this->render('index', [ ]);
+    }
+    /*public function actionIndex()
+    {
+        $searchModel = new CitizenSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }*/
+
+    /**
+     * Displays a single Citizen model.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Citizen model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Citizen();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->nik]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Citizen model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->nik]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Citizen model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Citizen model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $id
+     * @return Citizen the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Citizen::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
---
+++ frontend/views/reservation/show_antrian.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\helpers\Html;
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$this->title = 'Halaman Informasi Booking Antrian'
+?>
+<h1><?= Html::encode($this->title) ?></h1><br>
+<div style="text-align: center; font-weight: bold; font-size: 40px;">
+    <div>No Antrian</div>
+    <div><?= $model['no_antrian']; ?></div>
+    <div>Perkiraan Waktu Pemeriksaan</div>
+    <div><?= $model['time_exam']; ?></div>
+    <div><?=        yii\helpers\Html::a('Batal',['reservasi/cancel-reservation-member','id' => Yii::$app -> request -> get('id')]);?></div>
+</div>
+ 
