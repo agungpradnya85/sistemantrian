dfb269311683b1de9badc4d1e792fe5b4c02d6cf
agung pradnya <agungpradnya85@gmail.com>
2017-10-01 21:35:41 +0800

    split time_exam

--- database/antrianonline.sql
+++ database/antrianonline.sql
@@ -1,97 +1,187 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 15, 2017 at 06:06 PM
--- Server version: 10.1.22-MariaDB
--- PHP Version: 7.1.4
+/*
+SQLyog Community v12.4.3 (64 bit)
+MySQL - 10.1.22-MariaDB : Database - antrianonline
+*********************************************************************
+*/
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+/*!40101 SET NAMES utf8 */;
 
+/*!40101 SET SQL_MODE=''*/;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`antrianonline` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
---
--- Database: `antrianonline`
---
+USE `antrianonline`;
 
--- --------------------------------------------------------
+/*Table structure for table `citizen` */
 
---
--- Table structure for table `klinik`
---
+DROP TABLE IF EXISTS `citizen`;
+
+CREATE TABLE `citizen` (
+  `nik` varchar(20) NOT NULL,
+  `tipe` varchar(20) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `kelurahan` varchar(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `propinsi` varchar(50) NOT NULL,
+  `kabupaten` varchar(50) NOT NULL,
+  `kecamatan` varchar(50) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  PRIMARY KEY (`nik`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `citizen` */
+
+insert  into `citizen`(`nik`,`tipe`,`tanggal_lahir`,`kelurahan`,`nama`,`propinsi`,`kabupaten`,`kecamatan`,`alamat`) values 
+('5171030112850020','penduduk','1985-12-01','Panjer','Agung Pradnya','Bali','Denpasar','Denpasar Selatan','Jalan Tukad Pancoran Gang 2A No : 7'),
+('5171030112850021','penduduk','2017-09-01','Panjer','Mirah Dwidaputri','Bali','Denpasar','Denpasar Selatan','Jalan Tukad Pancoran Gang 2A No : 7'),
+('5171030112850022','penduduk','1998-12-25','Panjer','Ananta Tridaputra','Bali','Denpasar','Denpasar Selatan','Jalan Tukad Pancoran Gang 2A No : 7'),
+('5171030112850023','penduduk','1988-12-27','Tanguntiti','Satcitananda','Bali','Tabanan','Selemadeg Kangin','Jalan Beraban'),
+('5171030112850024','penduduk','1989-12-22','Tanguntiti','Nadia','Bali','Tabanan','Selemadeg Kangin','Jalan Beraban'),
+('5171030112850025','penduduk','1989-07-06','Nusa Dua','Panji Nugraha','Bali','Badung','Kuta','Jalan Sahadewa'),
+('5171030112850026','penduduk','2017-09-22','Nusa Dua','Gede Yudhistira','Bali','Badung','Kuta','Jalan Sahadewa');
+
+/*Table structure for table `faskes` */
+
+DROP TABLE IF EXISTS `faskes`;
+
+CREATE TABLE `faskes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) NOT NULL,
+  `tipe` enum('puskesmas','rumahsakit') NOT NULL,
+  `kecamatan` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `kecamatan` (`kecamatan`),
+  CONSTRAINT `faskes_ibfk_1` FOREIGN KEY (`kecamatan`) REFERENCES `kecamatan` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+
+/*Data for the table `faskes` */
+
+insert  into `faskes`(`id`,`nama`,`tipe`,`kecamatan`) values 
+(1,'Puskesmas Pembantu Desa Sobangan','puskesmas',4),
+(2,'Puskesmas Pembantu Desa Pererenan','puskesmas',4),
+(5,'Rumah Sakit Umum Daerah Kabupaten Badung Mangusada','rumahsakit',3),
+(6,'Puskesmas Pembantu Desa Buduk','puskesmas',4),
+(7,'Puskesmas Pembantu Desa Cemagi','puskesmas',4),
+(8,'Puskesmas Pembantu Desa gulingan','puskesmas',4),
+(9,'Puskesmas Pembantu Desa Kuwum','puskesmas',4),
+(10,'Puskesmas Pembantu Desa Lukluk','puskesmas',4),
+(11,'Puskesmas Pembantu Desa Penarungan','puskesmas',4),
+(12,'Puskesmas Pembantu Desa Pererenan','puskesmas',4),
+(13,'Puskesmas Pembantu Desa Sembung','puskesmas',4),
+(14,'Puskesmas Pembantu Desa Werdi Bhuwana','puskesmas',4);
+
+/*Table structure for table `kecamatan` */
+
+DROP TABLE IF EXISTS `kecamatan`;
+
+CREATE TABLE `kecamatan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `slug` varchar(50) NOT NULL DEFAULT '0',
+  `nama` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+/*Data for the table `kecamatan` */
+
+insert  into `kecamatan`(`id`,`slug`,`nama`) values 
+(1,'abiansemal','ABIANSEMAL'),
+(2,'petang','PETANG'),
+(3,'kuta','KUTA'),
+(4,'mengwi','MENGWI'),
+(5,'kuta-utara','KUTA UTARA'),
+(6,'kuta-selatan','KUTA SELATAN');
+
+/*Table structure for table `klinik` */
+
+DROP TABLE IF EXISTS `klinik`;
 
 CREATE TABLE `klinik` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `kode_klinik` varchar(3) NOT NULL,
-  `nama_klinik` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `nama_klinik` varchar(30) NOT NULL,
+  `id_faskes` int(11) NOT NULL,
+  `jumlah_poli` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_faskes` (`id_faskes`),
+  CONSTRAINT `klinik_ibfk_1` FOREIGN KEY (`id_faskes`) REFERENCES `faskes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `klinik`
---
+/*Data for the table `klinik` */
 
-INSERT INTO `klinik` (`id`, `kode_klinik`, `nama_klinik`) VALUES
-(1, 'A', 'POLI UMUM'),
-(2, 'B', 'Gigi');
+insert  into `klinik`(`id`,`kode_klinik`,`nama_klinik`,`id_faskes`,`jumlah_poli`) values 
+(1,'A','Poli Umum',1,5),
+(2,'B','Poli Gigi',2,8),
+(3,'A','Poli Umum',2,7),
+(4,'B','Poli Gigi',1,3),
+(5,'C','KIA',1,4),
+(6,'C','KIA',2,6),
+(8,'A','Poli Umum',5,7),
+(9,'B','Poli Gigi',5,5);
 
--- --------------------------------------------------------
+/*Table structure for table `klinik_map` */
 
---
--- Table structure for table `klinik_map`
---
+DROP TABLE IF EXISTS `klinik_map`;
 
 CREATE TABLE `klinik_map` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `no_antrian` varchar(11) NOT NULL,
   `id_klinik` int(11) NOT NULL,
-  `id_pasien` int(11) NOT NULL,
+  `id_pasien` varchar(20) NOT NULL,
   `tanggal` date NOT NULL,
   `status` smallint(3) NOT NULL DEFAULT '1' COMMENT 'status 0 : batal, status 1 : bisa hadir (default), 2 : sudah dipanggil',
-  `time_exam` varchar(20) NOT NULL
+  `time_exam` varchar(50) NOT NULL,
+  `time_exam_start` datetime DEFAULT NULL,
+  `time_exam_end` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_klinik` (`id_klinik`),
+  CONSTRAINT `klinik_map_ibfk_1` FOREIGN KEY (`id_klinik`) REFERENCES `klinik` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `klinik_map`
---
+/*Data for the table `klinik_map` */
 
-INSERT INTO `klinik_map` (`id`, `no_antrian`, `id_klinik`, `id_pasien`, `tanggal`, `status`, `time_exam`) VALUES
-(1, 'A1', 1, 7, '2017-08-15', 1, '08.00 - 10.00');
+/*Table structure for table `migration` */
 
--- --------------------------------------------------------
-
---
--- Table structure for table `migration`
---
+DROP TABLE IF EXISTS `migration`;
 
 CREATE TABLE `migration` (
   `version` varchar(180) NOT NULL,
-  `apply_time` int(11) DEFAULT NULL
+  `apply_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `migration`
---
+/*Data for the table `migration` */
 
-INSERT INTO `migration` (`version`, `apply_time`) VALUES
-('m000000_000000_base', 1501595649),
-('m130524_201442_init', 1501595654);
+insert  into `migration`(`version`,`apply_time`) values 
+('m000000_000000_base',1501595649),
+('m130524_201442_init',1501595654);
 
--- --------------------------------------------------------
+/*Table structure for table `noncitizen` */
 
---
--- Table structure for table `user`
---
+DROP TABLE IF EXISTS `noncitizen`;
+
+CREATE TABLE `noncitizen` (
+  `identity_number` varchar(50) NOT NULL,
+  `noncitizen_name` varchar(50) NOT NULL,
+  `birth_date` date NOT NULL,
+  `address` varchar(100) NOT NULL,
+  PRIMARY KEY (`identity_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `noncitizen` */
+
+insert  into `noncitizen`(`identity_number`,`noncitizen_name`,`birth_date`,`address`) values 
+('3674041711860004','Chandrayana Putra','1985-10-02','Kapal');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `nama` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `no_ktp` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
@@ -104,90 +194,23 @@ CREATE TABLE `user` (
   `role` varchar(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'member',
   `status` smallint(6) NOT NULL DEFAULT '10',
   `created_at` int(11) NOT NULL,
-  `updated_at` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `updated_at` int(11) NOT NULL,
+  `faskes_access` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `password_reset_token` (`password_reset_token`),
+  KEY `faskes_access` (`faskes_access`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`faskes_access`) REFERENCES `faskes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `user`
---
+/*Data for the table `user` */
 
-INSERT INTO `user` (`id`, `username`, `nama`, `no_ktp`, `alamat`, `no_hp`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `role`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'umami', 'Mamai', '', '', '', 'WSOYPtYyS9K3-fRfnbCS8JOoPRHuNMnH', '$2y$13$4z8g/Biap9ptDNrbHpBHSuFT/XVT5wJaG4GdKm0nAioFVcV2ajMF6', NULL, 'umami@gmail.com', 'member', 10, 1501597871, 1501597871),
-(2, 'aaa', 'bbb', '', '', '', 'G8jF0LIowtr2qjH_6s0-l0Tp8NXEMkA8', '$2y$13$SpKWbCDRDdMnYtNWygd7k.RP3zyF6RizvDcF.K1tx0pIiodWYsxEa', NULL, NULL, 'member', 10, 1501601937, 1501601937),
-(3, 'bbb', 'bababa', '111119999999', 'bh', '77777ooooooo', 'X72u40Nt9b2XDI0yKfD6EG4EriLIszhv', '$2y$13$SFfJYnojQxtVh/.cu0NIpeNiZxDl0LSvitgTjUTbYEWBFA7lF934y', NULL, NULL, 'member', 10, 1501602693, 1501602693),
-(4, 'gayatri', 'gayatri', '008999999999999', 'dalung', '081558800262', '8nw-y9fcJmiCMhp9MeRaEpx_MN3TcsbA', '$2y$13$udI.KVixooUY6wV2TywvB.rEwCv8/HZCW29oKEDmgS/o.31B3MZnS', NULL, NULL, 'member', 10, 1501603146, 1501603146),
-(5, 'agungpradnya', 'agungpradnya', '00000000000000000', 'tukad pancoran', '081558800262', 'OW_OBoojLCdPYJooBwGx5EeiPeaZ_0UV', '$2y$13$hmvnGzmBv96xulxcT1M7bOnKdc5aQMT6AWlFfCGIydN7rgO0l9PCW', NULL, NULL, 'admin', 10, 1501603591, 1501603591),
-(7, 'madesaguna', 'madesaguna', '99999999999999', 'panjer', '9999999999999', 'iDF1v445IOlIpPsH5C29DstZJ8A5FRWW', '$2y$13$TWACv1nibiH9rIf75YbkBeVElcS0ZhB7NPWVXr9eoOU9e68BOAdbS', NULL, NULL, 'member', 10, 1501603644, 1501603644),
-(8, 'andikaputra', 'andikaputra', '222344567788', 'Jalan Tukad Pancoran Gang 2 A NO 7', '081558800262', 'jY-tgQU7Q5lz0rC1QuXRAXAHau4sCiuW', '$2y$13$WA3R3iXvsZYo2ZXwavNrseK6GrTm1wq2RCKCPdnIzLIXwRsB9BF5C', NULL, NULL, 'member', 10, 1501663700, 1501663700),
-(9, 'wiryawan', 'wiryawan', '08566789978', 'Jalan Indrajaya', '0876668768', '55OpKzKSoIjy7HIBfO6wzoywA0yHtf6Y', '$2y$13$Xo4j7RD3tPxBsSLSJM65cOkEfWkfFqmAitDF4Ag5lip042xSs/Mue', NULL, NULL, 'member', 10, 1501740402, 1501740402),
-(10, 'mirahdwidaputri', 'mirahdwidaputri', '7869944403983849', 'jalan tukad pancoran gang 2 A No 7', '081222343432', 'Z-sVX79VH0J43Q5EH2OKNkW2lB78GnKX', '$2y$13$k0WvVLy0mVO8ruqfDpNKiupkESjxjFoJrzRdY8VVliFmH.lTqURii', NULL, NULL, 'member', 10, 1502256354, 1502256354),
-(11, 'irfanbachdim', 'irfanbachdim', '8883333333333', 'gianyar', '0812343234', 'Qsad43IgdezCuoMul8TMJWunq2Ykh-he', '$2y$13$muts6GbG188SJx9JG8YotucrJ6vMLFxoNBn3RNFDafGbIhvRZpXYW', NULL, NULL, 'member', 10, 1502262051, 1502262051),
-(12, 'jenifer', 'jenifer', '876798773345', 'tukad pancoran', '0876676577', '1E1fU2isP1dr8oo8kguM56NVo0TOQp4w', '$2y$13$i9QrEqVS.Wf9.sIiq72NKekfz8k0NA6QsSG7LCpu8GWqPCIaG3CaK', NULL, NULL, 'member', 10, 1502264536, 1502264536),
-(13, 'dddd', 'fff', '333', '222', '4444', 'YJA5HcIpNlDtpvkxBqZwMlCgxABESebv', '$2y$13$qpHl8hGbUQHxNZ4Z0lFpu.hj3wMrp3jbfhM.XHgbYwCCobjEhPBa2', NULL, NULL, 'member', 10, 1502369111, 1502369111),
-(14, '', 'fff', '333', '222', '4444', 'NDxPIVFtStNseV5TCnBHpaWHFjb6Tuzw', '$2y$13$J1NbjjKZFncm1qcxWbYHVuvTWEO1ZK.zmK8HgmX.geefJTbjFZw4O', NULL, NULL, 'member', 10, 1502369254, 1502369254),
-(15, 'mas_poltak', 'fff', '333', '222', '4444', 'a_R6yQ3yiRmKH1itsAGexSC4hwOHxLdF', '$2y$13$G45FAVgrMXaGSyDWrHR5sODjalyWo6ETVEHLY8M.yH9VwEzz..ZCO', NULL, NULL, 'member', 10, 1502369539, 1502369539);
+insert  into `user`(`id`,`username`,`nama`,`no_ktp`,`alamat`,`no_hp`,`auth_key`,`password_hash`,`password_reset_token`,`email`,`role`,`status`,`created_at`,`updated_at`,`faskes_access`) values 
+(1,'agungpradnya','agungpradnya','888777666555','Jalan Tukad Pancoran Gang 2A No 7','081558800262','BF2Mg-1d7Ye7ciNz6L74UpAekm3R9gaO','$2y$13$ASXXEjlBkLz0rdmXnwkAy.X0MwawAAXorSYhw6utiMPQTLUm0zTEG',NULL,NULL,'operator',10,1502968725,1502968725,NULL),
+(2,'superadmin','superadmin','12345656788900','planet neptunus','0833421345','lnybcBwuBOaXbsdQzYJv604Ug_Sw8qXn','$2y$13$.2H774/uKffvdY3t7BC4Tubs7pm7QQESlpg9DLe1AtdzKrAhJCMYi',NULL,NULL,'admin',10,1502969483,1502969483,NULL),
+(3,'gayatri','gayatri','8736495807','Dalung Permai','08123456788','2bSREf-bn1T0EhYXRBZ2oTO5qT7IbgVG','$2y$13$rxgeDavhawmG10BfESA6q.wvmS.KBSfPRNUnXpFb22ZZtRMd2HgJm',NULL,NULL,'operator',10,1502969628,1502969628,1);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `klinik`
---
-ALTER TABLE `klinik`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `klinik_map`
---
-ALTER TABLE `klinik_map`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_klinik` (`id_klinik`);
-
---
--- Indexes for table `migration`
---
-ALTER TABLE `migration`
-  ADD PRIMARY KEY (`version`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `password_reset_token` (`password_reset_token`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `klinik`
---
-ALTER TABLE `klinik`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `klinik_map`
---
-ALTER TABLE `klinik_map`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `klinik_map`
---
-ALTER TABLE `klinik_map`
-  ADD CONSTRAINT `klinik_map_ibfk_1` FOREIGN KEY (`id_klinik`) REFERENCES `klinik` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
