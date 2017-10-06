2e542c628c3b9396b44a926928d1037defa46bde
made saguna <madesaguna@users.noreply.github.com>
2017-10-06 18:59:52 +0800

    set default timezone to yii's config

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,8 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    // set default timezone GMT+8
+    'timezone' => 'Asia/Makassar',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
